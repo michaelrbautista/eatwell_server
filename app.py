4e3e0a5aa4079984fd9283ce30189dda23f95053
michael-bautista <mrbautistadev@gmail.com>
2025-05-03 10:42:08 -0700

    fixed prompt and second adjective

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ async def chat_with_openai(topic: str, user_religion: Optional[str] = None):
     ]
 
     entrepreneurs = [
-        "Steve Jobs", "Naval Ravikant", "Paul Graham", "Elon Musk", "Walt Disney", "Alex Hormozi", "Walt Disney", "Phil Knight", "David Ogilvy", "Goerge Lucas", "Edwin Catmull", "Thomas Edison", "Phil Jackson", "Jeff Bezos", "James Dyson", "Bill Hewitt", "Peter Thiel", "Levi Strauss", "Edwin Land", "Ray Dalio", "Richard Branson", "Marc Andreessen", "Cornelius Vanderbilt", "Herb Kelleher", "John Bogle", "Howard Hughes", "Yvon Chouinard", "Malcolm McLean", "Benjamin Franklin", "Marc Rich", "Coco Chanel", "Kirk Kerkorian", "Henry Kaiser", "Conrad Hilton", "Daniel Ludwig", "Charles Goodyear", "Stan Lee", "Andrew Carnegie", "Charlie Munger", "Henry Royce", "Thomas J. Watson", "Warren Buffet", "Henry Singleton", "Enzo Ferrari", "Carrol Shelby", "Bill Walsh", "Sol Price", "Frank Lloyd Wright", "Chung Ju-yung", "the Wright brothers", "the Dodge brothers", "Billy Durant", "Albert Champion", "Larry Ellison", "Henry Leland", "Walter Chrysler", "Estee Lauder", "Alexander Graham Bell", "J.P. Morgan", "Bill Gates", "Arnold Schwarzenegger", "Ernest Shackleton", "William Randolph Hearst", "Milton Hershey", "Sam Colt", "Frederick Smith", "Katherin Graham", "Bill Bowerman", "Andy Grove", "Dr. Seuss", "Chuch Yeager", "Jony Ive", "Isadore Sharp", "Jim Casey", "Isambard Kingdom Brunel", "Michael Dell", "Steven Spielberg", "Stephen King", "Paul Van Doren", "Siggi Wilzig", "Charles de Gaulle", "Michael Bloomberg", "Sidney Harman", "William Rosenberg", "Jay Z", "Kanye West", "Harry Snyder", "Rick Rubin", "Mark Leonard", "Socrates", "Henry Goldman", "Jimi Hendrix", "Paul Orfalea", "Ralph Lauren", "Brunello Cucinelli", "David Packard", "Ray Krock", "Rose Blumkin", "James Cameron", "Christopher Nolan", "L'Ebe Bugatti", "Alistar Urquhart", "Sam Zemurray", "Ted Turner", "Les Schwab", "Christian Dior", "Dietrich Mateschitz", "Brad Jacobs", "Quentin Tarantino", "Hans Wilsdorf", "John Mackey", "Sam Zell", "Jensen Huang", "Jerry Jones", "Todd Graves", "Akio Morita"
+        "Steve Jobs", "Naval Ravikant", "Paul Graham", "Elon Musk", "Walt Disney", "Alex Hormozi", "Walt Disney", "Phil Knight", "David Ogilvy", "Goerge Lucas", "Edwin Catmull", "Thomas Edison", "Phil Jackson", "Jeff Bezos", "James Dyson", "Bill Hewitt", "Peter Thiel", "Levi Strauss", "Edwin Land", "Ray Dalio", "Richard Branson", "Marc Andreessen", "Cornelius Vanderbilt", "Herb Kelleher", "John Bogle", "Howard Hughes", "Yvon Chouinard", "Malcolm McLean", "Benjamin Franklin", "Marc Rich", "Coco Chanel", "Kirk Kerkorian", "Henry Kaiser", "Conrad Hilton", "Daniel Ludwig", "Charles Goodyear", "Stan Lee", "Andrew Carnegie", "Charlie Munger", "Henry Royce", "Thomas J. Watson", "Warren Buffet", "Henry Singleton", "Enzo Ferrari", "Carrol Shelby", "Bill Walsh", "Sol Price", "Frank Lloyd Wright", "Chung Ju-yung", "Billy Durant", "Albert Champion", "Larry Ellison", "Henry Leland", "Walter Chrysler", "Estee Lauder", "Alexander Graham Bell", "J.P. Morgan", "Bill Gates", "Arnold Schwarzenegger", "Ernest Shackleton", "William Randolph Hearst", "Milton Hershey", "Sam Colt", "Frederick Smith", "Katherin Graham", "Bill Bowerman", "Andy Grove", "Dr. Seuss", "Chuch Yeager", "Jony Ive", "Isadore Sharp", "Jim Casey", "Isambard Kingdom Brunel", "Michael Dell", "Steven Spielberg", "Stephen King", "Paul Van Doren", "Siggi Wilzig", "Charles de Gaulle", "Michael Bloomberg", "Sidney Harman", "William Rosenberg", "Jay Z", "Kanye West", "Harry Snyder", "Rick Rubin", "Mark Leonard", "Socrates", "Henry Goldman", "Jimi Hendrix", "Paul Orfalea", "Ralph Lauren", "Brunello Cucinelli", "David Packard", "Ray Krock", "Rose Blumkin", "James Cameron", "Christopher Nolan", "L'Ebe Bugatti", "Alistar Urquhart", "Sam Zemurray", "Ted Turner", "Les Schwab", "Christian Dior", "Dietrich Mateschitz", "Brad Jacobs", "Quentin Tarantino", "Hans Wilsdorf", "John Mackey", "Sam Zell", "Jensen Huang", "Jerry Jones", "Todd Graves", "Akio Morita"
     ]
 
     religion = [
@@ -76,7 +76,7 @@ async def chat_with_openai(topic: str, user_religion: Optional[str] = None):
     random_first_adjective = random.choice(first_adjectives)
 
     second_adjectives = [
-        "discipline", "perseverance", "grit", "tenacity", "work ethic", "sacrifice", "dedication", "endurance"
+        "discipline", "perseverance", "grit", "dedication", "endurance"
     ]
 
     random_second_adjective = random.choice(second_adjectives)
@@ -91,7 +91,7 @@ async def chat_with_openai(topic: str, user_religion: Optional[str] = None):
                 },
                 {
                     "role": "user", 
-                    "content": f"Give me a short, {random_first_adjective} quote from {random_person} about self improvement that highlights {random_second_adjective}. Do not return a quote about war itself, do not include quotation marks, and do not include any interpretations."
+                    "content": f"Give me a random, short, {random_first_adjective} quote from {random_person} about life that highlights {random_second_adjective}. Do not return a quote about war itself, do not include quotation marks, and do not include any interpretations."
                 }
             ],
             temperature=0.9,
